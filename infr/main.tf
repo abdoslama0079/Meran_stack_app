b0e7212f1c356b96e41d683b11ab9891a2276c09
Abdelrahman Salama <abdoabdoslama509@gmail.com>
2026-03-18 15:01:30 +0200

    Fix resource group name depending

--- infr/main.tf
+++ infr/main.tf
@@ -7,17 +7,15 @@ module "Vnet" {
   source = "./modules/Vnet"
 
   location    = var.location
-  rg_name     = var.rg_name
+  rg_name     = azurerm_resource_group.rg_name_tf
   aks_storage = module.StorageAccount.aks_storage_id
-  depends_on  = [azurerm_resource_group.rg_name_tf, module.StorageAccount]
+  depends_on  = [module.StorageAccount]
 }
 
 module "StorageAccount" {
   source        = "./modules/StorageAccount"
   location      = var.location
-  rg_name       = var.rg_name
-
-  depends_on = [azurerm_resource_group.rg_name_tf]
+  rg_name       = azurerm_resource_group.rg_name_tf
 }
 
 resource "azurerm_storage_account_network_rules" "lockdown" {
@@ -31,14 +29,13 @@ module "ACR" {
   source = "./modules/ACR"
 
   acr_location = var.location
-  rg_name      = var.rg_name
-  depends_on   = [azurerm_resource_group.rg_name_tf]
+  rg_name      = azurerm_resource_group.rg_name_tf
 }
 
 module "KeyVault" {
   source      = "./modules/Key vault"
   kv_location = var.location
-  rg_name     = var.rg_name
+  rg_name     = azurerm_resource_group.rg_name_tf
 }
 
 module "AKS" {
@@ -47,7 +44,7 @@ module "AKS" {
   aks_location = var.location
   acr_id       = module.ACR.acr_id
   subnet       = module.Vnet.aks_subnet_id
-  rg_name      = var.rg_name
+  rg_name      = azurerm_resource_group.rg_name_tf
   rg_id        = azurerm_resource_group.rg_name_tf.id
   kv_aks_id    = module.KeyVault.key_vault_id
 
